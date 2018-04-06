183c76fbacfb08fa72766505b84d844b63d06784
malaf4129 <malaf4129@7HFM6Y1.Student.UGDSB.ED>
2018-04-06 13:52:09 -0400

    WAYNE GRETZKY

---
+++ src/waynegretzky/WayneGretzky.java
@@ -0,0 +1,27 @@
+/*
+*Mathew Lafontaine
+*WayneGretzky.java
+*This project prints
+*Wayne Gretzky is 99 to screen
+*2/28/2018
+ */
+
+package waynegretzky;
+
+/**
+ *
+ * @author malaf4129
+ */
+public class WayneGretzky {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String name = "Wayne Gretzky";
+        int playerNumber = 99;
+        //This SOP adds a numeric to a String variable
+        System.out.println(name + " is number " + playerNumber);
+    }
+    
+}
